c90166a779d98ca127d35762689075fb77c352bb
PedroSt28 <stracierepedrohenrique@gmail.com>
2025-09-25 20:04:51 -0300

    adicionando novas paginas

--- senai-notes-angular/src/app/app.routes.ts
+++ senai-notes-angular/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginScreen } from './user-module/login-screen/login-screen';
+import { NewUserScreen } from './user-module/new-user-screen/new-user-screen';
+import { NotesScreen } from './notes-screen/notes-screen';
 
-export const routes: Routes = [{path:"login-screen", loadComponent:()=>LoginScreen}];
+export const routes: Routes = [{path:"login-screen", loadComponent:()=>LoginScreen}, {path:"newUser", loadComponent:()=>NewUserScreen},  {path:"Central", loadComponent:()=>NotesScreen}];
