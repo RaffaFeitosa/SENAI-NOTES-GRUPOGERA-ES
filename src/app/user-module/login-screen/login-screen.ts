751a758228617a8bbde1f21a3f02c7e8da71352a
RaffaFeitosa <rafael.feitosa91.silva@gmail.com>
2025-09-26 19:39:40 -0300

    Atualiuzacao do Repositorio para publicacao

---
+++ src/app/user-module/login-screen/login-screen.ts
@@ -0,0 +1,93 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-screen',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login-screen.html',
+  styleUrl: './login-screen.css'
+})
+export class LoginScreen {
+  loginForm: FormGroup;
+
+  emailErrorMessege: string;
+  passwordErrorMessege: string;
+constructor(private fb: FormBuilder, private cd: ChangeDetectorRef) {
+    //Quando a tela iniciar.
+
+    //Inicia o formulario
+    //Cria o campo obrigatório de email.
+    //Cria o campo obrigatório de senha.
+    this.loginForm = this.fb.group({
+
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]]
+
+    });
+
+    this.emailErrorMessege = "";
+    this.passwordErrorMessege = "";
+
+  }
+
+  async onLoginClick() {
+
+    //alert("Botão de login clicado.");
+    this.emailErrorMessege = "";
+    this.passwordErrorMessege = "";
+
+
+    console.log("Email", this.loginForm.value.email);
+    console.log("Password", this.loginForm.value.password);
+
+    if (this.loginForm.value.email == "") {
+
+
+      this.emailErrorMessege = "O Campo de e-mail é obrigatório."
+      return;
+    }
+
+    if (this.loginForm.value.password == "") {
+      this.passwordErrorMessege = "O Campo de Senha é obrigatório."
+      return;
+    }
+
+
+
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+      method: "POST", //ENVIAR,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password,
+      })
+    });
+
+    console.log("STATUS CODE", response.status)
+
+    if (response.status >= 200 && response.status <= 299) {
+
+      let json = await response.json();
+
+
+      console.log("JSON", json)
+
+      let meuToken = json.accessToken;
+      let meuId = json.user.id;
+
+      localStorage.setItem("meuToken", meuToken);
+      localStorage.setItem("meuId", meuId);
+
+      window.location.href = "Central"
+
+
+
+    }
+
+    this.cd.detectChanges();
+  }
+}
+
+
---
+++ src/app/user-module/new-user-screen/new-user-screen.ts
@@ -0,0 +1,96 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-user-screen',
+  imports: [ReactiveFormsModule],
+  templateUrl: './new-user-screen.html',
+  styleUrl: './new-user-screen.css'
+})
+export class NewUserScreen {
+
+  newUserScreen: FormGroup;
+
+  nameErrorMessege: string;
+  emailErrorMessege: string;
+  passwordErrorMessege: string;
+
+
+  constructor(private fb: FormBuilder, private cd: ChangeDetectorRef) {
+    this.newUserScreen = this.fb.group({
+
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+      name: ["", [Validators.required]],
+
+    });
+
+    this.nameErrorMessege = "";
+    this.emailErrorMessege = "";
+    this.passwordErrorMessege = "";
+
+  }
+
+  async onEnterClick() {
+
+    //alert("Botão de login clicado.");
+    this.nameErrorMessege = "";
+    this.emailErrorMessege = "";
+    this.passwordErrorMessege = "";
+
+    console.log("Name", this.nameErrorMessege);
+    console.log("Email", this.newUserScreen.value.email);
+    console.log("Password", this.newUserScreen.value.password);
+
+    if (this.newUserScreen.value.name == "") {
+      this.nameErrorMessege = "O Campo nome é obrigatório"
+      return;
+    }
+
+    if (this.newUserScreen.value.email == "") {
+      this.emailErrorMessege = "O Campo de e-mail é obrigatório."
+      return;
+    }
+
+    if (this.newUserScreen.value.password == "" ) {
+      this.passwordErrorMessege = "O Campo de Senha é obrigatório."
+      return;
+
+//    } else if (this.newUserScreen.value.password.length <= 6){
+  //    this.passwordErrorMessege = ""
+    }
+
+
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/users", {
+      method: "POST", //ENVIAR,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: this.newUserScreen.value.name,
+        email: this.newUserScreen.value.email,
+        password: this.newUserScreen.value.password,
+      })
+    });
+
+    console.log("STATUS CODE", response.status)
+
+    if (response.status >= 200 && response.status <= 299) {
+
+      let json = await response.json();
+
+      console.log("JSON", json)
+
+      window.location.href = "login-screen"
+
+
+    } else {
+     "Erro inseperado tente novamente mais tarde"
+
+      //alert("Acesso Negado")
+    }
+
+    this.cd.detectChanges();
+  }
+}
+
