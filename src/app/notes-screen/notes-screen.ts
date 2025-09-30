46ba727a9e688c7505b8d4a839ef05a73dcfec87
PedroSt28 <stracierepedrohenrique@gmail.com>
2025-09-29 21:56:53 -0300

    colocando TS na tela

--- src/app/notes-screen/notes-screen.ts
+++ src/app/notes-screen/notes-screen.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+
+interface INota{
+  nome :  string
+
+}
 
 @Component({
   selector: 'app-notes-screen',
@@ -8,4 +14,27 @@ import { Component } from '@angular/core';
 })
 export class NotesScreen {
 
+  novaNota: INota = {
+    nome: ""
+  }!;
+
+  async criarNota() {
+  
+    const nomeNota = prompt("qual o nome da Nota?")
+    console.log (nomeNota);
+
+    if(!nomeNota){
+
+      alert("criar nome descente")
+      return
+
+    }
+
+    this.novaNota.nome = nomeNota;
+
+  
+  };
+
 }
+
+
