fe1bd23945d9bf84730e1d3be106d4e0547cad7b
PedroSt28 <stracierepedrohenrique@gmail.com>
2025-10-01 21:55:06 -0300

    ts das notas

--- src/app/notes-screen/notes-screen.ts
+++ src/app/notes-screen/notes-screen.ts
@@ -1,8 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
-interface INota{
-  nome :  string
+
+interface INota {
+  nome: string;
+  UserId: string
 
 }
 
@@ -14,27 +17,90 @@ interface INota{
 })
 export class NotesScreen {
 
-  novaNota: INota = {
-    nome: ""
+  notaSelecionada: INota;
+  notas: INota[];
+  novaNota: INota = { nome: "", UserId: "meuId" };
 
-  }!;
+  constructor(private http: HttpClient, private cd: ChangeDetectorRef) {
+
+    this.notaSelecionada = null!;
+      this.notas = [];
+       
+
+    }
 
   async criarNota() {
-  
-    const nomeNota = prompt("qual o nome da Nota?")
-    console.log (nomeNota);
 
-    if(!nomeNota){
+    const nomeNota = prompt("qual o nome da Nota?")
+
+    if (!nomeNota) {
 
       alert("criar nome descente")
       return
 
     }
 
-    this.novaNota.nome = nomeNota;
+    const novoChatObj = {
 
-  
+      chatTitle: nomeNota,
+      userId: localStorage.getItem("meuId"),
+
+    }
+
+    let novoChatResponse = await firstValueFrom(this.http.post("https://senai-gpt-api.azurewebsites.net/chats", novoChatObj, {
+      headers: {
+        "content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("meuToken")
+      }
+
+    })) as INota;
+
+      await this.getNotas();
+
+
   };
+
+  async getNotas(){
+
+    let response = await firstValueFrom(this.http.get("https://senai-gpt-api.azurewebsites.net/chats",{
+      headers:{
+
+        "Authorization": "Bearer " + localStorage.getItem("meuToken")
+        
+
+      }
+
+      }))as INota[];
+
+    if (response) {
+
+      console.log("notas", response);
+
+      let userId = localStorage.getItem("meuId");
+
+      response = response.filter(chat => chat.UserId == userId);
+
+      
+      this.notas = response as []
+    }
+
+      
+  }
+  async onNoteClick(noteClicado : INota){
+
+    console.log("nota clicada"), noteClicado
+    this.notaSelecionada = noteClicado
+    //logca para buscar mensagens
+
+    let response = await firstValueFrom(this.http.get("https://senai-gpt-api.azurewebsites.net/messages?chatId=",{
+      
+    }
+
+    ))
+
+    this.cd.detectChanges();
+
+  }
 
 }
 
