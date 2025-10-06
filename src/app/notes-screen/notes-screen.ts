95f0d3c3787b1e2ae869b45178116cbf7beaeb18
PedroSt28 <stracierepedrohenrique@gmail.com>
2025-10-06 17:46:22 -0300

    adicionando tarefas

--- src/app/notes-screen/notes-screen.ts
+++ src/app/notes-screen/notes-screen.ts
@@ -1,40 +1,42 @@
-import {HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { firstValueFrom } from 'rxjs';
 
 
 interface INota {
   titulo: string;
   UserId: string;
-  id : ""
-
-
+  id: "";
+  descricao: string;
 }
 
 @Component({
   selector: 'app-notes-screen',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './notes-screen.html',
   styleUrl: './notes-screen.css'
 })
 export class NotesScreen {
 
+  tituloInput = new FormControl();
   notaSelecionada: INota;
   notas: INota[];
-  novaNota: INota = { titulo: "", UserId: "meuId", id : "" };
+  novaNota: INota = { titulo: "", UserId: "meuId", id: "", descricao: "" };
 
   constructor(private http: HttpClient, private cd: ChangeDetectorRef) {
 
     this.notaSelecionada = null!;
-      this.notas = [];
-       
+    this.notas = [];
 
-    }
 
-    ngOnInit() {
-      // Executado quando a tela carrega
-      this.getNotas();
-    }
+  }
+
+  ngOnInit() {
+    // Executado quando a tela carrega
+    this.getNotas();
+  }
 
   async criarNota() {
 
@@ -49,11 +51,11 @@ export class NotesScreen {
 
     const novoChatObj = {
 
-      titulo : nomeNota,
-      usuarioId : localStorage.getItem("meuId"),
+      titulo: nomeNota,
+      usuarioId: localStorage.getItem("meuId"),
       tags: [],
-      descricao : "",
-      imagemURL : "",
+      descricao: "",
+      imagemURL: "",
 
     }
 
@@ -65,22 +67,22 @@ export class NotesScreen {
 
     })) as INota;
 
-      await this.getNotas();
+    await this.getNotas();
 
 
   };
 
-  async getNotas(){
+  async getNotas() {
 
-    let response = await firstValueFrom(this.http.get("http://localhost:3000/notas",{
-      headers:{
+    let response = await firstValueFrom(this.http.get("http://localhost:3000/notas", {
+      headers: {
 
         "Authorization": "Bearer " + localStorage.getItem("meuToken")
-        
+
 
       }
 
-      }))as INota[];
+    })) as INota[];
 
     if (response) {
 
@@ -90,32 +92,70 @@ export class NotesScreen {
 
       // response = response.filter(chat => chat.UserId == userId);
 
-      
+
       this.notas = response as []
       this.cd.detectChanges();
     }
 
 
-      
+
   }
-  async onNoteClick(noteClicado : INota){
+  async onNoteClick(noteClicado: INota) {
 
     console.log("nota clicada"), noteClicado
     this.notaSelecionada = noteClicado
+    console.log(this.notaSelecionada)
+    this.tituloInput.setValue(this.notaSelecionada.titulo)
     //logca para buscar mensagens
-
-    let response = await firstValueFrom(this.http.get("http://localhost:3000/notas?notaId=" + this.notaSelecionada.UserId,{
-
-      
-    }
-
-    ))
-
-    this.cd.detectChanges();
 
   }
 
+  async onNoteSave() {
+    this.notaSelecionada.titulo = this.tituloInput.value;
+    let response = await firstValueFrom(this.http.put("http://localhost:3000/notas/" + this.notaSelecionada.id, this.notaSelecionada)) as INota[];
+
+    if (response) {
+
+      console.log("atualizado", response);
+      let userId = localStorage.getItem("meuId");
+      // response = response.filter(chat => chat.UserId == userId);
+      this.cd.detectChanges();
+    }
+  }
+
+  async deletarNotaSelecionada() {
+
+    let confirmation = confirm("deseja realmente apagar nota" + this.notaSelecionada.titulo + "?")
+    if (!confirmation) {
+      return;
+
+    }
+
+    try {
+      let deleteResponse = await firstValueFrom(this.http.delete("http://localhost:3000/notas/" + this.notaSelecionada.id, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("meuToken")
+        }
+      }
+      )) as INota;
+
+    } catch (error) {
+      console.log("Erro no delete: " + error);
+    }
+
+    this.notaSelecionada = null!;
+    this.getNotas();
+  }
+
+  async cancelarNota() {
+    this.notaSelecionada = null!
+    console.log("nenhuma nota selecionada")
+  }
+
+
 }
+
 
 
 
@@ -125,4 +165,4 @@ export class NotesScreen {
 
 //getNotes
 //OnNoteclick
-//OnNoteSave
+//OnNoteSa
